b3261227195951a99f3949e6e0c1675f5e355dd4
Faunie Szeloczei and Nathan Mayer <faunie@>
2017-06-14 19:09:05 -0700

    Add nested forEach to create array cards, Add html and manipulate DOM with prepend to ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 //back-end
-var suits = ["of Hearts","of Clubs","of Spades","of Diamonds"];
+var suits = [" of Hearts"," of Clubs"," of Spades"," of Diamonds"];
 var ranks = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
+
+var cards = [];
+
+suits.forEach(function(suit) {
+  ranks.forEach(function(rank) {
+    cards.push(rank + suit);
+  });
+});
+
+
+//front-end
+$(document).ready(function() {
+  cards.forEach(function(card) {
+    $("ul").prepend("<li>" + card + "</li>");
+  });
+});
